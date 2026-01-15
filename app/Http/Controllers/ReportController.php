ebec6ecda736c467b5805b6416be471f38b9c215
MorphyZ <danilashastin@mail.ru>
2026-01-15 11:17:20 +0500

    update controleler reports

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Report;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
-    public function index() {}
+    public function index()
+    {
+        $reports = Report::all();
+        return view('report.index', compact('reports'));
+    }
 }
